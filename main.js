34535ccff0a0cc418017904af061f3a7b1a6d210
Leitino <alessandro.leita89@gmail.com>
2018-04-17 17:56:43 +0200

    piccoli passi

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 $(function() {
+
   $('#button').click(function() {
     var select = $('#select').val()
     $.ajax({
       url:"https://www.boolean.careers/api/array/basket?n="+select,
       method:"GET",
       success:function(data) {
-        console.log(data.response)
+        var listaGiocatori = []
+        var giocatori = data.response
+        listaGiocatori.push(giocatori)
+        console.log(listaGiocatori)
+        for (var i = 0; i < giocatori.length; i++) {
+          console.log(giocatori[i])
 
+        }
       }
-
-
     })
   })
 
