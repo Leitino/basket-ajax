dc4d9f10ba0ccaffe3b96fc8e044612f7dee62b3
Leitino <alessandro.leita89@gmail.com>
2018-04-17 17:11:11 +0200

    preseguo

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 $(function() {
+  $('#button').click(function() {
+    var select = $('#select').val()
+    $.ajax({
+      url:"https://www.boolean.careers/api/array/basket?n="+select,
+      method:"GET",
+      success:function(data) {
+        console.log(data.response)
 
-  
+      }
+
+
+    })
+  })
 
 })
